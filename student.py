13baf7a4692c62dcb43e041a57f413918140d167
bhimangouda <patilbhimangoud42@gmail.com>
2025-10-16 15:34:44 +0530

    save to bca5

---
+++ student.py
@@ -0,0 +1,9 @@
+name = input("enter the name:")
+usn = input("enter usn:")
+div = input("enter the div:")
+marks = input("enter the marks:")
+
+print("\n user details")
+print(f"name: {name}")
+print(f"usn: {usn}")
+print(f"div: {div}")
